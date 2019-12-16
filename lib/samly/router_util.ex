3c303d67221e5c3ca16f3d8313cbdcc7e5b07666
bzbzh <vincent@cstar.io>
2019-12-16 17:06:29 +0100

    logging test

--- lib/samly/router_util.ex
+++ lib/samly/router_util.ex
@@ -87,6 +87,12 @@ defmodule Samly.RouterUtil do
       url =
         :esaml_binding.encode_http_redirect(idp_url, signed_xml_payload, :undefined, relay_state)
 
+      Logger.error("url for send_saml_request: #{url} : #{inspect(url)}")
+      url2 =
+        :esaml_binding.encode_http_redirect(idp_url, signed_xml_payload, :undefined, "")
+      Logger.error("url TEST for send_saml_request: #{url} : #{inspect(url2)}")
+
+
       conn |> redirect(302, url)
     else
       nonce = conn.private[:samly_nonce]
